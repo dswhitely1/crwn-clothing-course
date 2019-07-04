77e1c41c9adc32a7e638d693a39f4d300417ac54
Donald Whitely <dswhitely1@gmail.com>
2019-07-03 20:47:58 -0400

    Shop Display built

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import {withRouter} from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import './menu-item.styles.scss';
 
-const MenuItem = ({title, imageUrl, size, linkUrl, history, match}) => (
-    <div className={`menu-item ${size}`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
-        <div style={{backgroundImage: `url(${imageUrl})`}} className='background-image'/>
-        <div className='content'>
-            <h1 className='title'>{title.toUpperCase()}</h1>
-            <span className='subtitle'>SHOP NOW</span>
-        </div>
+const MenuItem = ({ title, imageUrl, size, linkUrl, history, match }) => (
+  <div className={`menu-item ${size}`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
+    <div style={{ backgroundImage: `url(${imageUrl})` }} className='background-image'/>
+    <div className='content'>
+      <h1 className='title'>{title.toUpperCase()}</h1>
+      <span className='subtitle'>SHOP NOW</span>
     </div>
-)
+  </div>
+);
 
 export default withRouter(MenuItem);
